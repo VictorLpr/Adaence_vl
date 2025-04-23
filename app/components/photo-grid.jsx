40d1032b1b56c2a40eeec763e636fe5fb6a62b72
VictorLpr <victorlepron@gmail.com>
2025-04-23 22:19:08 +0200

    update photo grid

--- app/components/photo-grid.jsx
+++ app/components/photo-grid.jsx
@@ -5,7 +5,7 @@ export default function PhotoGrid({ images }) {
     return (
       <div className="photo-grid">
         {images.map((image, index) => (
-          <div className="photo-item" key={index}>
+          <div className={`photo-item item${index < 4 ? index%2: (index%2)-1}`} key={index} style={{gridColumn: `${index % 4 + 1}`}}>
             <img src={image} alt={`person-${index}`} />
           </div>
         ))}
--- app/page.jsx
+++ app/page.jsx
@@ -6,13 +6,13 @@ import PhotoGrid from './components/photo-grid';
 export default function HomePage() {
   const images = [
     './images/danie-franco-ClHY-KyvI1M-unsplash.jpg',
-    './images/vladimir-soares-z_8Jqe0Cc-s-unsplash.jpg',
     './images/tim-mossholder-FRPbQvAwY10-unsplash.jpg',
-    './images/shimo-yann-7nTFrV1xQGE-unsplash.jpg',
     './images/tatiana-zanon-MMhazsT2wtM-unsplash.jpg',
-    './images/michael-matveev-Bk8zT4gLLt0-unsplash.jpg',
     './images/quentin-mahe-a6jCS61X1Hg-unsplash.jpg',
-    './images/mohammad-samir-huHXKc_usoA-unsplash.jpg'
+    './images/vladimir-soares-z_8Jqe0Cc-s-unsplash.jpg',
+    './images/shimo-yann-7nTFrV1xQGE-unsplash.jpg',
+    './images/mohammad-samir-huHXKc_usoA-unsplash.jpg',
+    './images/michael-matveev-Bk8zT4gLLt0-unsplash.jpg'
   ]
 
   return (
