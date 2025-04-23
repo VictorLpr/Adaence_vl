468e91c311e79039e8ed8d1587521ea4d8e7d5da
VictorLpr <victorlepron@gmail.com>
2025-04-23 13:06:50 +0200

    clean pages.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,7 @@
-import LikeButton from './components/like-button';
 import Navbar from './components/navbar';
 import './styles/globals.css';
 import Searchbar from './components/search-bar';
-import Image from 'next/image';
 
-
-function Header({ title }) {
-  return <h1>{title ? title : 'Default title'}</h1>;
-}
 
 export default function HomePage() {
  
