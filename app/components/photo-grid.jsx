27982c054b1a6d0fb2b390a3c4552608b43306f6
VictorLpr <victorlepron@gmail.com>
2025-04-24 09:47:24 +0200

    fix photo grid

--- app/components/photo-grid.jsx
+++ app/components/photo-grid.jsx
@@ -1,12 +1,20 @@
 'use client'
 import '../styles/photo-grid.css';
 
+const areaNames = [
+  'img1', 'img2', 'img3', 'img4',
+  'img5', 'img6', 'img7', 'img8'];
+
 export default function PhotoGrid({ images }) {
     return (
       <div className="photo-grid">
         {images.map((image, index) => (
-          <div className={`photo-item item${index < 4 ? index%2: (index%2)-1}`} key={index} style={{gridColumn: `${index % 4 + 1}`}}>
-            <img src={image} alt={`person-${index}`} />
+         <div
+          key={index}
+          className="photo-item"
+          style={{ gridArea: areaNames[index] }}
+        >
+            <img src={image} alt={`person-${index+1}`} />
           </div>
         ))}
       </div>
