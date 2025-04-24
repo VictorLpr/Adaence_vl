82f86feddb7a88bc2efdea6542c2b2c1f72521d2
VictorLpr <victorlepron@gmail.com>
2025-04-24 14:56:47 +0200

    update search bar

--- app/components/search-bar.jsx
+++ app/components/search-bar.jsx
@@ -1,25 +1,34 @@
 'use client'
 import '../styles/search-bar.css'
+import { usePathname } from 'next/navigation'
 
-export default function Searchbar() {
+export default function Searchbar(props) {
     const options = ["Un repas", "Un café/thé", "Une promenade", "Une sortie culturelle", "Autre activité"]
+    const pathname = usePathname()
 
     return (
-        <form action="/visite" method="GET" className="search-bar">
+        <form action="/visite"  className="search-bar">
+            <div className='up-search-bar'>
+
             <div style={{ display: 'flex', flexDirection: 'column' }}>
                 <label htmlFor="moment">Moment à partager</label>
-                <select name="moment" id="moment">
+                <select name="moment" id="moment" defaultValue={props.moment || ""}>
                     <option value="">Tous les moments possibles</option>
-                    {options.map(option => (
-                    <option value={option}>{option}</option>
+                    {options.map((option, index) => (
+                        <option key={index} value={option}>{option}</option>
                     ))}
                 </select>
             </div>
             <div style={{ display: 'flex', flexDirection: 'column' }}>
                 <label htmlFor="localisation">Localisation</label>
-                <input type="text" name="localisation" placeholder="Votre ville" />
+                <input type="text" name="localisation" placeholder="Votre ville" defaultValue={props.localisation || ""}/>
             </div>
             <input type="submit" value="Rechercher 🔎" />
+            </div>
+            <div className='results' style={{display : pathname == '/visite' ? 'flex' : 'none'}}>
+                    <p> moments trouvés</p>
+                    <p>Réintialiser les filtres</p>
+            </div>
         </form>
     )
 }
--- app/visite/page.js
+++ app/visite/page.js
@@ -1,7 +1,17 @@
+"use client"
 import '../styles/globals.css';
+import Searchbar from '../components/search-bar';
+import { useSearchParams } from 'next/navigation';
+ 
 
 
 export default function Visite() {
+          const searchParams = useSearchParams();
+          const moment = searchParams.get('moment');
+          const localisation = searchParams.get('localisation')
     return (<div className="main">
+        <div className='header'>
+            <Searchbar moment={moment} localisation={localisation}/>
+        </div>
     </div>)
 }
