5ee54279bfc52d04273837ead9aecd3eacce7827
VictorLpr <victorlepron@gmail.com>
2025-04-29 11:30:34 +0200

    update : testimony

--- app/components/comment.jsx
+++ app/components/comment.jsx
@@ -3,17 +3,17 @@ import '../styles/comment.css'
 
 export default function Comment({ name, duration, quote, imageUrl }) {
     return (
-      <div className="comment-card">
-        <div className="comment-avatar">
-          <img src={imageUrl} alt={name} />
+        <div className="comment-card">
+            <div className="comment-avatar">
+                <img src={imageUrl} alt={name} />
+                <div className="comment-content">
+                    <p className="comment-name">{name}</p>
+                    <p className="comment-role">Bénévole depuis {duration}</p>
+                </div>
+            </div>
+            <blockquote className="comment-quote">
+                ❝{quote}❞
+            </blockquote>
         </div>
-        <div className="comment-content">
-          <p className="comment-name">{name}</p>
-          <p className="comment-role">Bénévole depuis {duration}</p>
-          <blockquote className="comment-quote">
-            ❝{quote}❞
-          </blockquote>
-        </div>
-      </div>
     );
-  }
+}
