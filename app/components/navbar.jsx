6ce0429f6afa54932f1746dabf3cb029be5d89be
VictorLpr <victorlepron@gmail.com>
2025-04-29 15:42:53 +0200

    add : lock nav bar if already on page

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -3,23 +3,27 @@
 import Link from "next/link";
 import { House , Accessibility, BookOpen,HandHelping} from 'lucide-react';
 import Don from "./don";
+import { usePathname } from 'next/navigation'
+
 
 
 export default function Navbar() {
+        const pathname = usePathname()
+    
     return (<nav className="flex">
         <div className="title">🍵Ada<span>ence</span></div>
         <ul>
             <li>
-            <Link href="/"><House />Accueil</Link>
+            <Link href="/" className = {pathname == "/" ? "disabled" : ""}><House />Accueil</Link>
             </li>
             <li>
-            <Link href="/visite"><Accessibility />Je rends visite</Link>
+            <Link href="/visite" className = {pathname == "/visite" ? "disabled" : ""} ><Accessibility />Je rends visite</Link>
             </li>
             <li>
             <Link href="/guide"><BookOpen />Guide du partage</Link>
             </li>
             <li>
-            <Link href="/benevole"><HandHelping />Devenir bénévole</Link>
+            <Link href="/benevole" className = {pathname == "/benevole" ? "disabled" : ""}><HandHelping />Devenir bénévole</Link>
             </li>
 
         </ul>
